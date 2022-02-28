d54168a84508fc1cafe3873ff2d2dbbe0db82102
Heet-Sheth <heetsheth0000@gmail.com>
2022-02-28 11:59:06 +0530

    Higher Order Compnent

---
+++ src/Components/ClickCounter.js
@@ -0,0 +1,25 @@
+import React from "react";
+import updatedComponent from "./index.js";
+import "./ClickCounter.css";
+function ClickCounter(props) {
+  const { name, count, incrementCount } = props;
+  return (
+    <div className="Container">
+      <h1>{name}</h1>
+      <input type="text" value={count} disabled />
+      <div className="innerContainer">
+        <button title="Decrement" onClick={() => incrementCount(-1)}>
+          &lt;
+        </button>
+        <button title="Increment" onClick={() => incrementCount(1)}>
+          &gt;
+        </button>
+        <button title="Reset" onClick={() => incrementCount(0)}>
+          ↺
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default updatedComponent(ClickCounter);
---
+++ src/Components/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const UpdatedComponent = (OriginalComponent) => {
+  function NewComponent(props) {
+    const [count, setCount] = useState(0);
+    const incrementCount = (abc) => {
+      setCount(abc === 0 ? 0 : count + abc);
+    };
+    return (
+      <OriginalComponent
+        name="DarkPro"
+        count={count}
+        incrementCount={incrementCount}
+      />
+    );
+  }
+
+  return NewComponent;
+};
+
+export default UpdatedComponent;
