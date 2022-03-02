8b32b8b9d225c3fb81c677c76da6c00695cc3969
Heet-Sheth <heetsheth0000@gmail.com>
2022-03-02 11:28:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from "react";
+import "./App.css";
+const Page = lazy(() => import("./Component/simplyPage.js"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense className="App" fallback={<div>Wait...</div>}>
+      <Page />
+    </Suspense>
   );
 }
 
